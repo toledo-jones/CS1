3ae910357d04f023d1dabb9a0966172595859bf6
toledo-jones <cody.peter.bogausch@gmail.com>
2024-01-25 10:29:03 -0500

    class work 1-25-2024

---
+++ Class_1-25-2024/name.py
@@ -0,0 +1,7 @@
+""" This program asks for a a name and age"""
+
+name = input("What is your name? ")
+age = int(input("What is your age?"))
+
+print(f"Your name is: {name}")
+print(f"Your age is: {age}")

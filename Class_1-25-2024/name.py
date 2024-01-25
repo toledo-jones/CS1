6e90356441d8c9c41105c385bd863950dab7eafb
toledo-jones <cody.peter.bogausch@gmail.com>
2024-01-25 10:41:58 -0500

    Finished classwork

--- Class_1-25-2024/name.py
+++ Class_1-25-2024/name.py
@@ -1,7 +1,13 @@
 """ This program asks for a a name and age"""
 
 name = input("What is your name? ")
-age = int(input("What is your age?"))
+age = int(input("What is your age? "))
 
 print(f"Your name is: {name}")
+print(f"The number of letters in your name is: {len(name)}")
+print(f"The first letter in your name is {name[0]}")
 print(f"Your age is: {age}")
+if age > 21:
+    print("Congratulations you are old enough to drink alochol!")
+else:
+    print("Sorry, you are not old enough to drink alcohol.")

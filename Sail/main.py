03af10b1c998afe50dd9779fdd1ab6bf05ba09ec
toledo-jones <cody.peter.bogausch@gmail.com>
2024-02-01 10:49:14 -0500

    worked on sail project/ left off on FISH POPULATION GROWTH

---
+++ Sail/main.py
@@ -0,0 +1,18 @@
+# Use this file to deal with sails editor
+
+INIT_POP_POND_1 = 10 
+INIT_POP_POND_2 = 25
+INIT_POP_POND_3 = 12
+INIT_POP_POND_4 = 30
+INIT_POP_POND_5 = 4
+
+YEARLY_GROWTH_POND_1 = 2.0
+YEARLY_GROWTH_POND_2 = 1.5
+YEARLY_GROWTH_POND_3 = 1.8
+YEARLY_GROWTH_POND_4 = 2.2
+YEARLY_GROWTH_POND_5 = 1.5
+
+YEARS = 3
+
+def pop_after_n_years(init_pop, yearly_growth, years):
+    <est. population> = <initial population> x (<yearly growth>^<years>) # pseudo

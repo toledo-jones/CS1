68ea422fc32dd37365194d2c353c6c9be3de5777
toledo-jones <cody.peter.bogausch@gmail.com>
2024-02-15 10:52:58 -0500

    class

--- Sail/Unit 3/text.py
+++ Sail/Unit 3/text.py
@@ -1,11 +1,12 @@
-nums = (5, 10) + (15, 20)
-if 3 in nums:
-    print ("Spring")
-elif max(nums) > 50:
-    print ("Summer")
-elif sum(nums) > 100:
-    print ("Fall")
-elif (min(nums) + 5) > 15:
-    print ("Winter")
-elif nums.count(5) < 3:
-    print ("Spring")
+# Test string
+a = "abcdef"
+
+# Cleanest way to reverse the string. 
+"Zen-of-Python"
+
+# Display results: 
+print(f"{b} is b")
+
+# Proof
+# print(a[b])
+

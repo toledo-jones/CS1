214e223a76afd874b343e28a5988b3e3f7353cbb
toledo-jones <bogausch.cody@gmail.com>
2024-04-23 09:39:39 -0400

    Sync 2

--- Assignments/5/main.py
+++ Assignments/5/main.py
@@ -3,8 +3,72 @@
 
 """
 This module is a command line grading program
- Assignment Description:
-    foo
+Evaluation and Grading
+    Quizzes (20%)
+    Projects (50%)
+    Assignments (20%)
+    Class Participation (10%)
+Grading Scale:
+    A 93 – 100
+    A- 90 – 92
+    B+ 87 – 89
+    B 83 – 86
+B- 80 – 82
+C+ 77 – 79
+C 73 – 76
+C- 70 – 72
+D 65 – 69
+F Below 65
+
+Output should be exactly this:
+
+CS 127 Grade Calculator
+
+Description (?)
+
+Enter name:
+Enter the number of Assignments: 5
+
+Enter grade for Assignment #1: 90
+Enter grade for Assignment #2: 56
+Enter grade for Assignment #3: 100
+Enter grade for Assignment #4: 98
+Enter grade for Assignment #5: 100
+
+Enter the number of Quizzes: 6
+
+Enter grade for Quiz #1: 90
+Enter grade for Quiz #2: 56
+Enter grade for Quiz #3: 100
+Enter grade for Quiz #4: 98
+Enter grade for Quiz #5: 100
+Enter grade for Quiz #6: 100
+
+Enter the number of Projects: 6
+
+Enter grade for Project #1: 90
+Enter grade for Project #2: 56
+Enter grade for Project #3: 100
+Enter grade for Project #4: 98
+Enter grade for Project #5: 100
+Enter grade for Project #6: 100
+
+How many class sessions did you miss this semester? 2
+
+
+Code/algorithm:
+Average the quiz scores, then multiply by .20
+Average the project scores, then multiply by .50
+Average the assignment scores, then multiply by .20
+Calculate participation grade (10%)
+Total all the calculations above
+Determine letter grade
+
+Print report
+
+
+
+
 
  Functions:
     main

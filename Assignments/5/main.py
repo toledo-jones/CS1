6546b75d2d374cfc00670637b3c60dea4d0cceaa
toledo-jones <bogausch.cody@gmail.com>
2024-04-23 09:34:25 -0400

    Started assignment 5

---
+++ Assignments/5/main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+This module is a command line grading program
+*****************************************************************************************************
+ Description:
+    foo
+*****************************************************************************************************
+
+ Functions:
+    main
+    timed_test
+    create_random_list
+
+
+
+ ChangeLog:    Cody Bogausch
+           (cody.bogausch@sunycgcc.edu)
+
+
+ Version 1.0
+   April 23, 2024
+
+"""
+
+__author__ = "Cody Bogausch"
+__version__ = "1.0"
+
+
+def main() -> None:
+    """
+    Entry point
+    """
+    ?
+
+# This file is executable:
+if __name__ == '__main__':
+    main()
